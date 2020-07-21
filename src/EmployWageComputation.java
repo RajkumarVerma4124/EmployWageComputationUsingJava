793e5cfb7872bf269ca35ed8891700e30cbebd04
RAJ <vermar4142@gmail.com>
2020-07-21 17:43:45 +0530

    Added:- Calculated Part Time Employ Wage For Employ Who is Present

--- src/EmployWageComputation.java
+++ src/EmployWageComputation.java
@@ -6,21 +6,29 @@ public class EmployWageComputation
 		System.out.println();
 		System.out.println("Calculate Daily Employee Wage(UC2)");
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter The Total Daily Hour : ");
+		int dailyHours = sc.nextInt();
+		System.out.print("Enter The Wage Per Hour : ");
+		int wagePerHours = sc.nextInt();
 		//constant
 		final int fullTime=1;
+		final int partTime=2;
 		//computation
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		if ( empCheck == fullTime ) {
-			System.out.println("Employee is Present");
-			System.out.print("Enter The Total Daily Hour : ");
-			int dailyHours = sc.nextInt();
-			System.out.print("Enter The Wage Per Hour : ");
-			int wagePerHours = sc.nextInt();
+			System.out.println("Employee is Present For Full Day");
 			int fullDayWage = wagePerHours * dailyHours;	
 			System.out.println("Employee daily wage is :"+fullDayWage);
 		}
-		else{
-			System.out.println("Employee is Absent");
+		else if ( empCheck == partTime ) {
+			System.out.println("Employee is Present For Half Day");
+			int partTimeHours=wagePerHours / 2;
+			int partTimeWage = partTimeHours * dailyHours;	
+			System.out.println("Employee part time wage is :"+partTimeWage);
+		}
+		else {
+			System.out.println("Employee is Absent For The day");
+			dailyHours = 0;
 		}
 		sc.close();
 		
