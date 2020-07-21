6c55699ceff3b30b1869b733fbf7e974ae26d32f
RAJ <vermar4142@gmail.com>
2020-07-21 17:34:49 +0530

    Added:-Displaying Welcome Message To the program in master branch

---
+++ src/EmployWageComputation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class EmployWageComputation
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		System.out.println();
+	}
+
+}
