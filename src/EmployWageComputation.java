61be9b61b4f9bf58c6878782cab1712fe2cd58ca
RAJ <vermar4142@gmail.com>
2020-07-21 17:36:22 +0530

    Added:- Checking Employ Present or Absent Using Random

--- src/EmployWageComputation.java
+++ src/EmployWageComputation.java
@@ -1,9 +1,17 @@
 import java.util.*;
 public class EmployWageComputation
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		System.out.println();
+		System.out.println("Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check(UC1)");
+		final int fullTime=1;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if ( empCheck == fullTime )
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
