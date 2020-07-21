b75283b813488e3c3da8acbe2a4835c0294d9ede
RAJ <vermar4142@gmail.com>
2020-07-21 18:12:10 +0530

    Added:- Calculated Wages Until a Condition Of Total working Day or Hours

--- src/EmployWageComputation.java
+++ src/EmployWageComputation.java
@@ -10,30 +10,33 @@ public class EmployWageComputation
 		System.out.print("Enter The Wage Per Hour : ");
 		int wagePerHours = sc.nextInt();
 		//constant
-		final int fullTime=1, partTime=2, dayPerMonth = 20;
+		final int fullTime=1, partTime=2, dayPerMonth = 20, totalHours = 100;
 		//variable
-		int dailyWage, monthlyWage = 1, dailyEmpHours =0;
+		int dailyWage, monthlyWage = 0, dailyEmpHours = 0, hours = 0, day = 0;
 		//computation
-		System.out.println("Solving using Switch Case Statement (UC4)");
+		System.out.println("Calculated Wages till a condition of working hours or days is reached for a month (UC6)");
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case fullTime:
-				dailyEmpHours = dailyHours;
-				System.out.println("Employ Wage For Full Time is :"+(dailyEmpHours*wagePerHours));
-				break;
-			case partTime:
-				dailyEmpHours = dailyHours / 2;
-				System.out.println("Employ Wage For Part Time is :"+(dailyEmpHours*wagePerHours));
-				break;
-			default:
-				dailyEmpHours = 0;
-				System.out.println("Employ Is Absent For Today with wage :" +dailyEmpHours);
-				break;
+		while ( hours < totalHours && day < dayPerMonth ) {
+			empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case fullTime:
+					dailyEmpHours = dailyHours;
+					break;
+				case partTime:
+					dailyEmpHours = dailyHours / 2;
+					break;
+				default:
+					dailyEmpHours = 0;
+					break;
+			}
+			hours += dailyEmpHours;
+			day++;
+			dailyWage = dailyEmpHours * wagePerHours;
+			monthlyWage += dailyWage;
+			System.out.println("Day : " +day+ "  Hours : " +hours+ "  DailyWage : " +dailyWage+ "  TotalWage : " +monthlyWage);
 		}
+		System.out.println("Employee Total monthly wage is : "+monthlyWage+ "  and Total Hours : "+hours+"  Total Day is : "+day);
 		System.out.println();
-		System.out.println("Calculating Wages for a Month - Assume 20 Working Day per Month (UC5)");	
-		dailyWage = wagePerHours * dailyHours;
-		monthlyWage = dayPerMonth * dailyWage;
 		System.out.println("Employee monthly wage is "+monthlyWage);
 		sc.close();
 	}
