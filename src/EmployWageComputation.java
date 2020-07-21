4432d1c479bee884c52c91da3f4f458f36da3ecb
RAJ <vermar4142@gmail.com>
2020-07-21 18:00:03 +0530

    Added:- Calculated Wages For a Month

--- src/EmployWageComputation.java
+++ src/EmployWageComputation.java
@@ -10,10 +10,9 @@ public class EmployWageComputation
 		System.out.print("Enter The Wage Per Hour : ");
 		int wagePerHours = sc.nextInt();
 		//constant
-		final int fullTime=1;
-		final int partTime=2;
+		final int fullTime=1, partTime=2, dayPerMonth = 20;
 		//variable
-		int dailyEmpHours = 0;
+		int dailyWage, monthlyWage = 1, dailyEmpHours =0;
 		//computation
 		System.out.println("Solving using Switch Case Statement (UC4)");
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -31,6 +30,11 @@ public class EmployWageComputation
 				System.out.println("Employ Is Absent For Today with wage :" +dailyEmpHours);
 				break;
 		}
+		System.out.println();
+		System.out.println("Calculating Wages for a Month - Assume 20 Working Day per Month (UC5)");	
+		dailyWage = wagePerHours * dailyHours;
+		monthlyWage = dayPerMonth * dailyWage;
+		System.out.println("Employee monthly wage is "+monthlyWage);
 		sc.close();
 	}
 
