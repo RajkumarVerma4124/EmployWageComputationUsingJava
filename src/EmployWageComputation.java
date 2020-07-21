3f36369f5abd0d6b95dc5bba43f0a8cd353b0de7
RAJ <vermar4142@gmail.com>
2020-07-21 19:59:14 +0530

    Added:-Ability to save the TotalWage for Each Company using HashMap

--- src/EmployWageComputation.java
+++ src/EmployWageComputation.java
@@ -3,22 +3,22 @@ import java.util.*;
 public class EmployWageComputation {
     public static final int fullTime = 1;
     public static final int partTime = 2;
+    	
+		public final String company;
+	    public final String name;
+	    public final int wagePerHours;
+	    public final int dailyHours;
+	    public final int dayPerMonth;
+	    public final int totalHours;
 
-    public final String company;
-    public final String name;
-    public final int wagePerHours;
-    public final int dailyHours;
-    public final int dayPerMonth;
-    public final int totalHours;
-
-    public EmployWageComputation(String company, String name, int wagePerHours, int dailyHours, int dayPerMonth, int totalHours) {
-        this.company = company;
-        this.name = name;
-        this.wagePerHours = wagePerHours;
-        this.dailyHours = dailyHours;
-        this.dayPerMonth = dayPerMonth;
-        this.totalHours = totalHours;
-    }
+	    public EmployWageComputation(String company, String name, int wagePerHours, int dailyHours, int dayPerMonth, int totalHours) {
+	        this.company = company;
+	        this.name = name;
+	        this.wagePerHours = wagePerHours;
+	        this.dailyHours = dailyHours;
+	        this.dayPerMonth = dayPerMonth;
+	        this.totalHours = totalHours;
+	    }
 
     private int employMonthlyWage() {
         // variable
@@ -59,7 +59,7 @@ public class EmployWageComputation {
         
         System.out.print("Enter The No. Of Records You Want To Enter Max is 4 : ");
         int records = sc.nextInt();
-        
+        Map<String, Integer> companyWage = new HashMap<>();
         if( records <=4 ) {
 		    for(int i=1; i<=records; i++) {
 			    System.out.print("Enter The Name Of The Company : ");
@@ -78,28 +78,69 @@ public class EmployWageComputation {
 			    if(i == 1) {
 			    	EmployWageComputation companyOne = new EmployWageComputation(company, name, wagePerHours, dailyHours, dayPerMonth, totalHours);
 			    	System.out.println("Employ Name " + name + " Got Total monthly Wage Of Rs : " + companyOne.employMonthlyWage()+ " From Company Name " +companyOne.company);
+			    	companyWage.put(company, companyOne.employMonthlyWage());
 			    	System.out.println();
 			    }
 			    if(i == 2) {
 				    EmployWageComputation companyTwo = new EmployWageComputation(company, name, wagePerHours, dailyHours, dayPerMonth, totalHours);
 				    System.out.println("Employ Name " + name + " Got Total monthly Wage Of Rs : " + companyTwo.employMonthlyWage()+ " From Company Name " +companyTwo.company);
-				    sc.close();
+				    companyWage.put(company, companyTwo.employMonthlyWage());
+			    	System.out.println();
+
 			    }
 			    if(i == 3) {
 				    EmployWageComputation companyThree = new EmployWageComputation(company, name, wagePerHours, dailyHours, dayPerMonth, totalHours);
 				    System.out.println("Employ Name " + name + " Got Total monthly Wage Of Rs : " + companyThree.employMonthlyWage()+ " From Company Name " +companyThree.company);
-				    sc.close();
+				    companyWage.put(company, companyThree.employMonthlyWage());
+			    	System.out.println();
+
 			    }
 			    if(i == 4) {
 				    EmployWageComputation companyFour = new EmployWageComputation(company, name, wagePerHours, dailyHours, dayPerMonth, totalHours);
 				    System.out.println("Employ Name " + name + " Got Total monthly Wage Of Rs : " + companyFour.employMonthlyWage()+ " From Company Name " +companyFour.company);
-				    sc.close();
+				    companyWage.put(company, companyFour.employMonthlyWage());
+			    	System.out.println();
 			    }
 		    }
         }
         else {
 		    System.out.print("Entries Should be Less than or equal to 4 ");
         }
+   
+        while (true) {
+	        System.out.println("Company Wage Stored in array : " +companyWage);
+	        System.out.println("Enter The Company Name To get The Total Wage and press e to Exit ");
+	        String companyName = sc.next();
+	        String exit = "e";
+	        if (companyName.equals(exit)) {
+	        	System.exit(1);
+	        }
+	        else {
+	        System.out.println("Total Wage For The Company Name : " +companyName+ " "+companyWage.get(companyName));
+	        }  
+        }
     }
         
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
