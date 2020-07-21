10466b8ecbb66f626b39c7156f8ed656981bdef1
RAJ <vermar4142@gmail.com>
2020-07-21 17:40:37 +0530

    Added:- Calculated Daily Employ Wage For Employ Who is Present

--- src/EmployWageComputation.java
+++ src/EmployWageComputation.java
@@ -1,17 +1,29 @@
 import java.util.*;
 public class EmployWageComputation
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		System.out.println();
-		System.out.println("Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check(UC1)");
+		System.out.println("Calculate Daily Employee Wage(UC2)");
+		Scanner sc = new Scanner(System.in);
+		//constant
 		final int fullTime=1;
+		//computation
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if ( empCheck == fullTime )
+		if ( empCheck == fullTime ) {
 			System.out.println("Employee is Present");
-		else
+			System.out.print("Enter The Total Daily Hour : ");
+			int dailyHours = sc.nextInt();
+			System.out.print("Enter The Wage Per Hour : ");
+			int wagePerHours = sc.nextInt();
+			int fullDayWage = wagePerHours * dailyHours;	
+			System.out.println("Employee daily wage is :"+fullDayWage);
+		}
+		else{
 			System.out.println("Employee is Absent");
+		}
+		sc.close();
+		
 	}
 
 }
